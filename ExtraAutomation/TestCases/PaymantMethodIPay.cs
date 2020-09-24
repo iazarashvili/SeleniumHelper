19bb77c3728a2a7b576ff86ee28e1926e7e35f2e
I L o O O <ilia.azarashvili@gmail.com>
2020-09-24 15:38:21 +0400

    add payment Methods

---
+++ ExtraAutomation/TestCases/PaymantMethodIPay.cs
@@ -0,0 +1,26 @@
+﻿using ExtraAutomationTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtraAutomation.TestCases
+{
+    class PaymantMethodIPay : BaseClass
+    {
+        public static string SearchInput = "//div[@id='header']//div//div//div//div//input";
+        public static string SearchButton = "//div[@class='rounded-lg search-wrap w-100 h-60px w-100" +
+            " bg-gray-550 d-flex align-items-center']//child::button";
+        public static string AddProduct = "//button[text()=' კალათაში დამატება ']";
+        public static string Basket = "//span[text()='კალათა']";
+        public static string ShopButton = "//button[text()=' ყიდვა ']";
+
+        public static string PaymentByCardButton = "//span[text()='ბარათით გადახდა']";
+        public static string OrderCompleteButton = "//button[text()=' შეკვეთის დასრულება ']";
+
+        public void PaymantTestIpay()
+        {
+            var login = new Login(WebDriver);
+            login.TestLogin();
+        }
+    }
+}
---
+++ ExtraAutomation/TestCases/PaymentMethodBalance.cs
@@ -0,0 +1,29 @@
+﻿using ExtraAutomationTesting;
+using SingularQATestService;
+using SeleniumHelper.Base;
+using NUnit.Framework;
+
+namespace ExtraAutomation.TestCases
+{
+    [TestFixture]
+    class PaymentMethodBalance : BaseClass
+    {
+        public static string SearchInput = "//div[@id='header']//div//div//div//div//input";
+        public static string SearchButton ="//div[@class='rounded-lg search-wrap w-100 h-60px w-100" +
+            " bg-gray-550 d-flex align-items-center']//child::button";
+        public static string AddProduct = "//button[text()=' კალათაში დამატება ']";
+        public static string Basket = "//span[text()='კალათა']";
+        public static string ShopButton = "//button[text()=' ყიდვა ']";
+
+        public static string PaymentByCardButton = "//span[text()='ბარათით გადახდა']";
+        public static string OrderCompleteButton = "//button[text()=' შეკვეთის დასრულება ']";
+
+        [Test, Category("Paymant Method Balance")]
+        public void PaymantTestBalance()
+        {
+            var login = new Login(WebDriver);
+            login.TestLogin();
+            BaseMethods.SendKeys(WebDriver, ElementLocator.Xpath, SearchInput, "80509");
+        }
+    }
+}
