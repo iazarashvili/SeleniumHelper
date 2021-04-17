7b385ba7ac54fb30aee0e02ceea5064591f7a767
Ilia Azarashvili <i.azarashvili@area.ge>
2021-04-17 10:47:28 +0400

    Fix Driver problems

--- ExtraAutomation/BaseClass.cs
+++ ExtraAutomation/BaseClass.cs
@@ -19,29 +19,15 @@ namespace ExtraAutomation
         protected static IWebDriver WebDriver;
         private static ChromeOptions _chromeOptions;
         private static ExtentReports _extent;
-        private static ExtentTest _test;
+        private static readonly ExtentTest _test;
 
 
         [OneTimeSetUp]
         protected static void DoBeforeAllTheTest()
         {
-
-            var path = System.Reflection.Assembly.GetCallingAssembly().CodeBase;
-            if (path != null)
-            {
-                var actualPath = path.Substring(0, path.LastIndexOf("bin", StringComparison.Ordinal));
-                var projectPath = new Uri(actualPath).LocalPath;
-                Directory.CreateDirectory(projectPath + "Reports");
-                var reportPath = projectPath + @"Reports\ExtentReport.html";
-                var htmlReporter = new ExtentHtmlReporter(reportPath, ViewStyle.Default);
-                _extent = new ExtentReports();
-                _extent.AttachReporter(htmlReporter);
-
-            }
-
-            _extent.AddSystemInfo("Host Name", "LocalHost");
-            _extent.AddSystemInfo("Environment", "QA");
-            _extent.AddSystemInfo("UserName", "TestUser");
+            _chromeOptions = new ChromeOptions();
+            _chromeOptions.AddArgument("start-maximized");
+            WebDriver = new ChromeDriver(@"D:\Projects\SeleniumHelper\SeleniumHelper\Driver", _chromeOptions);
         }
 
         // სრულდება ერთხელ ყველა ტესტის შემდეგ
@@ -49,7 +35,7 @@ namespace ExtraAutomation
         [OneTimeTearDown]
         protected static void DoAfterAllTheTests()
         {
-            _extent.Flush();
+            
         }
 
         //სრულდება ერთხელ ყველა ტესტის შემდეგ
@@ -57,27 +43,6 @@ namespace ExtraAutomation
         [TearDown]
         protected static void DoAfterEach()
         {
-            var status = TestContext.CurrentContext.Result.Outcome.Status;
-            var stacktrace = string.IsNullOrEmpty(TestContext.CurrentContext.Result.StackTrace) ? ""
-                : string.Format("{0}", TestContext.CurrentContext.Result.StackTrace);
-            Status logstatus = Status.Pass;
-            switch (status)
-            {
-                case TestStatus.Failed:
-                    logstatus = Status.Fail;
-                    break;
-                case TestStatus.Inconclusive:
-                    logstatus = Status.Warning;
-                    break;
-                case TestStatus.Skipped:
-                    logstatus = Status.Skip;
-                    break;
-                default:
-                    logstatus = Status.Pass;
-                    break;
-            }
-            _test.Log(logstatus, "Test ended with " + logstatus + stacktrace);
-
             WebDriver.Quit();
         }
         // ყველა ტესტის წინ
@@ -85,11 +50,9 @@ namespace ExtraAutomation
         [SetUp]
         protected static void DoBeforeEach()
         {
-            _extent.CreateTest(TestContext.CurrentContext.Test.Name);
-            WebDriver = new ChromeDriver(@"D:\GitProj\SeleniumHelper\SeleniumHelper\Driver", _chromeOptions, TimeSpan.FromMinutes(12));
+            
             WebDriver.Navigate().GoToUrl("https://extra.ge/");
-            WebDriver.Manage().Window.Maximize();
-            _chromeOptions.PageLoadStrategy = PageLoadStrategy.Normal;
+            
             BaseMethods.ShouldLocate(WebDriver, "https://extra.ge/");
         }
     }
