e6d00a7eea51d47961d6c24897e358efc1426285
Ilia Azarashvili <i.azarashvili@area.ge>
2021-04-17 14:05:08 +0400

    ასდ

--- ExtraAutomation/BaseClass.cs
+++ ExtraAutomation/BaseClass.cs
@@ -31,41 +31,8 @@ namespace ExtraAutomation
 
             _chromeOptions = new ChromeOptions();
             _extent = new ExtentReports();
+            _chromeOptions.AddArgument("start-maximized");
 
-
-            //var actualPath = path.Substring(0, path.LastIndexOf("bin", StringComparison.Ordinal));
-            //var projectPath = new Uri(actualPath).LocalPath;
-            //Directory.CreateDirectory(projectPath + "Reports");
-            string reportPaths = @"C:\Users\Administrator\Desktop\QALoger\ExtentReport.html";
-
-            try
-            {
-                if (Directory.Exists(reportPaths))
-                {
-                    Console.WriteLine("That path exists already.");
-                    return;
-                }
-                DirectoryInfo di = Directory.CreateDirectory(reportPaths);
-                Console.WriteLine("The directory was created successfully at {0}.", Directory.GetCreationTime(reportPaths));
-
-                // Delete the directory.
-                di.Delete();
-                Console.WriteLine("The directory was deleted successfully.");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("The process failed: {0}", e.ToString());
-                throw;
-            }
-            //var reportPath = reportPaths + "ExtentReport.html";
-            var htmlReporter = new ExtentHtmlReporter(reportPaths, ViewStyle.Default);
-            _extent.AttachReporter(htmlReporter);
-
-
-
-            _extent.AddSystemInfo("Host Name", "LocalHost");
-            _extent.AddSystemInfo("Environment", "QA");
-            _extent.AddSystemInfo("UserName", "TestUser");
         }
 
         // სრულდება ერთხელ ყველა ტესტის შემდეგ
@@ -81,26 +48,7 @@ namespace ExtraAutomation
         [TearDown]
         protected static void DoAfterEach()
         {
-            var status = TestContext.CurrentContext.Result.Outcome.Status;
-            var stacktrace = string.IsNullOrEmpty(TestContext.CurrentContext.Result.StackTrace) ? ""
-                : string.Format("{0}", TestContext.CurrentContext.Result.StackTrace);
-            Status logstatus = Status.Pass;
-            switch (status)
-            {
-                case TestStatus.Failed:
-                    logstatus = Status.Fail;
-                    break;
-                case TestStatus.Inconclusive:
-                    logstatus = Status.Warning;
-                    break;
-                case TestStatus.Skipped:
-                    logstatus = Status.Skip;
-                    break;
-                default:
-                    logstatus = Status.Pass;
-                    break;
-            }
-            _test.Log(logstatus, "Test ended with " + logstatus + stacktrace);
+            
 
             WebDriver.Quit();
         }
@@ -109,7 +57,7 @@ namespace ExtraAutomation
         [SetUp]
         protected static void DoBeforeEach()
         {
-            _extent.CreateTest(TestContext.CurrentContext.Test.Name);
+            
             WebDriver = new ChromeDriver(@"D:\GitProj\SeleniumHelper\SeleniumHelper\Driver", _chromeOptions, TimeSpan.FromMinutes(12));
             WebDriver.Navigate().GoToUrl("https://extra.ge/");
             WebDriver.Manage().Window.Maximize();
